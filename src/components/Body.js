93b09e46feea96dbab28e82af44a12322650bda8
brtvsk <vadim.nakitnyak@gmail.com>
2020-01-24 15:41:04 +0200

    Add input lyrics request

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,48 +1,73 @@
 import React, {Component} from 'react';
+import {requestAudd} from './Logic'
 
 function Body() {
-  return (
-    <section className="App-body">
-      <Inputs />
-    </section>
-  );
+    return (
+        <section className="App-body">
+            <Inputs/>
+        </section>
+    );
 }
 
 function Inputs() {
-  return (
-    <div className="input">
-      <Listen />
-      <div className="or">
-        OR
-      </div>
-      <Type />
-    </div>
-  )
+    return (
+        <div className="input">
+            <Listen/>
+            <div className="or">
+                OR
+            </div>
+            <Type/>
+        </div>
+    )
 }
 
 function Listen() {
-  return (
-    <div className="listen">
-      <img src="./res/mic.svg" alt="" className="mic"></img>
-      <div className="audio-type-selection">
-        <button className="button">
-          Song
-        </button>
-        <button className="button">
-          Humming
-        </button>
-      </div>
-    </div>
-  )
+    return (
+        <div className="listen">
+            <img src="./res/mic.svg" alt="" className="mic"></img>
+            <div className="audio-type-selection">
+                <button className="button">
+                    Song
+                </button>
+                <button className="button">
+                    Humming
+                </button>
+            </div>
+        </div>
+    )
 }
-function Type() {
-  return (
-    <div className="type">
-      <img src="./res/lyrics.svg" alt="" className="lyrics"></img>
-      <textarea className="textarea" rows="2" placeholder="Your lyrics..." >
-      </textarea>
-    </div>
-  )
+
+class Type extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        requestAudd(null, 'findLyrics', this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="type">
+                <form onSubmit={this.handleSubmit}>
+                    <img src="./res/lyrics.svg" alt="" className="lyrics"></img>
+                    <textarea className="textarea" rows="2" placeholder="Your lyrics..." onChange={this.handleChange}/>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Body;  
