a095c94d2d54f98287fdd48f79ed890756e22356
brtvsk <vadim.nakitnyak@gmail.com>
2020-01-24 23:52:29 +0200

    Fix Score

--- src/components/Score.js
+++ src/components/Score.js
@@ -3,15 +3,11 @@ import React, {Component} from 'react';
 class Score extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			name: props.name,
-			score: props.score
-		};
 	}
 	
 	render() {
 		return <div className="score">
-			{this.state.name}:{this.state.score}
+			{this.props.name}:{this.props.score}
 		</div>;
 	}
 }
