663655a595212e5ab59e539ae01bee7f00718054
brtvsk <vadim.nakitnyak@gmail.com>
2020-01-24 15:28:53 +0200

    Fix requestAudd function

--- src/components/Logic.js
+++ src/components/Logic.js
@@ -2,12 +2,16 @@ import React from 'react';
 import 'mic-recorder-to-mp3' ;
 import $ from 'jquery';
 
+const AUDD_URL = "https://api.audd.io/"
+const KEY_TOKEN = "f540762d9f7dbfa0e9484b2e44f3d237"
+const RETURN_VALUE = "deezer"
+
 function requestAudd(file, method, q) {
     var data = new FormData();
     if (file != null)
         data.append("file", file);
-    data.append("return", "deezer");
-    data.append("api_token", "f540762d9f7dbfa0e9484b2e44f3d237");
+    data.append("return", RETURN_VALUE);
+    data.append("api_token", KEY_TOKEN);
     if (method != undefined)
         data.append('method', method)
 
@@ -15,14 +19,14 @@ function requestAudd(file, method, q) {
         data.append('q', q)
 
     $.ajax({
-        url: 'https://api.audd.io/',
+        url: AUDD_URL,
         data: data,
         cache: false,
         contentType: false,
         processData: false,
         method: 'POST',
         success: function (data) {
-            console.table(data);
+           return data;
         }
     })
 }
