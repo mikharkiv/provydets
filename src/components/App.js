f156d0bf4c34c005cf633b6c86bd922307d24f2f
brtvsk <vadim.nakitnyak@gmail.com>
2020-01-24 20:56:39 +0200

    Change App to class and add simple navigation

--- src/components/App.js
+++ src/components/App.js
@@ -6,24 +6,46 @@ import Header from './Header';
 import Footer from "./Footer";
 import AnswerScreen from './AnswerScreen';
 
-function App() {
-	// TODO: FOR TEST
-	const answerTestProps = {
-		attempt: 1,
-		songPreview: "https://img.discogs.com/dADBPB6TzEoBdGQFKQy-MhHBA_0=/fit-in/388x374/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2027877-1259432078.jpeg.jpg",
-		songTitle: "What a wounderful world",
-		songAuthor: "Louis Armstrong",
-	};
-	
-	
-	return (
-		<div className="App">
-			<Header />
-			<Body />
-			<AnswerScreen param={answerTestProps} />
-			<Footer/>
-		</div>
-	);
+class App extends React.Component{
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			component:<Body callbackFromParent={this.myCallback}/>
+		}
+	}
+
+	myCallback = (dataFromChild) => {
+		const answerTestProps = {
+			attempt: 1,
+			songPreview: "https://img.discogs.com/dADBPB6TzEoBdGQFKQy-MhHBA_0=/fit-in/388x374/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2027877-1259432078.jpeg.jpg",
+			songTitle: "What a wonderful world",
+			songAuthor: "Louis Armstrong",
+		};
+		switch (dataFromChild) {
+			case
+			"Body"
+			:
+				this.setState({component:<Body callbackFromParent={this.myCallback}/>})
+				break
+			case
+			"AnswerScreen"
+			:
+				this.setState({component:<AnswerScreen param={answerTestProps} callbackFromParent={this.myCallback}/>})
+				break
+		}
+	}
+
+
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                {this.state.component}
+                <Footer/>
+            </div>
+        );
+    }
 }
 
 export default App;
