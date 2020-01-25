f77c93ecc42a6e21c198cab61685513baba56a47
dmv302 <spaceshroom175@gmail.com>
2020-01-25 19:07:55 +0200

    [DEV] Finishing merge

--- src/components/App.js
+++ src/components/App.js
@@ -10,54 +10,37 @@ import SongPreview from './SongPreview';
 import WinScreen from './WinScreen';
 
 class App extends React.Component {
+	
 	constructor(props) {
 		super(props);
 		this.state = {
-			pc:0,
-			user:0
+			pc: 0,
+			user: 0
 		}
 	}
 
 	pointUp = (pointReceiver) => {
-		switch(pointReceiver){
+		switch (pointReceiver) {
 			case "pc":
-				let pc_points = this.state.pc+1;
-				this.setState({pc:pc_points});
+				let pc_points = this.state.pc + 1;
+				this.setState({ pc: pc_points });
 				break
 			case "user":
-				let user_points = this.state.user+1
-				this.setState({user:user_points});
+				let user_points = this.state.user + 1
+				this.setState({ user: user_points });
 				break
 		}
 	}
-  	// TODO: FOR TEST
-	const answerTestProps = {
-		attempt: 1,
-		songPreview: "https://img.discogs.com/dADBPB6TzEoBdGQFKQy-MhHBA_0=/fit-in/388x374/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2027877-1259432078.jpeg.jpg",
-		songTitle: "What a wounderful world",
-		songAuthor: "Louis Armstrong",
-	};
-	const songs = [
-		answerTestProps,
-		answerTestProps,
-		answerTestProps,
-		answerTestProps,
-		answerTestProps
-	];
-	
-  render() {
-	return (
-		<div className="App">
-			<Header />
-			{/*<Attempt />*/}
-			{/*<Body />*/}
-			{/*<AnswerScreen param={answerTestProps} />*/}
-			{/*<SongPreview song={answerTestProps} />*/}
-			<WinScreen won={true} winner="User" song={ answerTestProps } songs={ songs }/>
-			<Footer/>
-		</div>
-	);
-  }
+
+	render() {
+		return (
+			<div className="App">
+				<Header />
+				<Body />
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,9 +9,9 @@ class Header extends React.Component {
 	render() {
 		return (
 			<header className="App-header">
-				<Score name="pc" score={this.props.pc}/>
+				<Score name="pc" score={0}/>
 				<Buttons/>
-				<Score name="user" score={this.props.user}/>
+				<Score name="user" score={0}/>
 			</header>
 		);
 	}
