8a12b18f29092aad667edfb1ca2e233d94c8bf4a
brtvsk <vadim.nakitnyak@gmail.com>
2020-01-25 13:27:46 +0200

    Add Modal to Buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
 import Button from './Button';
+import Modal from 'react-modal'
 
+const customStyles = {
+	content : {
+		top                   : '50%',
+		left                  : '50%',
+		right                 : 'auto',
+		bottom                : 'auto',
+		marginRight           : '-50%',
+		transform             : 'translate(-50%, -50%)'
+	}
+};
 
 class Buttons extends Component {
 	constructor(props) {
@@ -8,11 +19,29 @@ class Buttons extends Component {
 		this.clickNewGameButton = this.clickNewGameButton.bind(this)
 		this.clickResetScores = this.clickResetScores.bind(this)
 		this.clickButtons = this.clickButtons.bind(this)
+		this.state = {
+			modalIsOpen: false
+		};
+
+		this.openModal = this.openModal.bind(this);
+		this.afterOpenModal = this.afterOpenModal.bind(this);
+		this.closeModal = this.closeModal.bind(this);
+		this.startNewGame = this.startNewGame.bind(this);
+	}
+
+	openModal() {
+		this.setState({modalIsOpen: true});
+	}
+
+	afterOpenModal() {
+	}
+
+	closeModal() {
+		this.setState({modalIsOpen: false});
 	}
 
 	clickNewGameButton(){
-		console.log(this)
-		console.log("NEW GAME STARTS")
+		this.openModal()
 	}
 	clickResetScores(){
 		console.log("RESET SCORES")
@@ -21,9 +50,21 @@ class Buttons extends Component {
 		console.log("BUTTON BUTTONS WS CLICKED")
 	}
 
+	startNewGame(){
+		this.closeModal()
+		console.log("NEW GAME STARTS")
+	}
+
 	render() {
 		return (
 			<div className="button-group">
+				<Modal isOpen={this.state.modalIsOpen} style={customStyles}>
+					<p className="txt">Are you sure you want to start a new game ?</p>
+					<div className="button-group">
+					<button className="button" onClick={this.closeModal}>NO</button>
+					<button className="button" onClick={this.startNewGame}>YES</button>
+					</div>
+				</Modal>
 				<Button type="new game" onClick={this.clickNewGameButton} />
 				<Button type="reset scores" onClick={this.clickResetScores}/>
 				<Button type="buttons"onClick={this.clickButtons} />
