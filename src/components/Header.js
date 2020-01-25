b1a01ebfc02eae8e37d443fcbbe5b31b6e37f945
brtvsk <vadim.nakitnyak@gmail.com>
2020-01-25 12:36:24 +0200

    Header to class

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,19 @@ import React from 'react';
 import Score from './Score';
 import Buttons from './Buttons';
 
-function Header(props) {
-	return (
-		<header className="App-header">
-			<Score name="pc" score={props.pc}/>
-			<Buttons />
-			<Score name="user" score={props.user}/>
-		</header>
-	);
+class Header extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return (
+			<header className="App-header">
+				<Score name="pc" score={this.props.pc}/>
+				<Buttons/>
+				<Score name="user" score={this.props.user}/>
+			</header>
+		);
+	}
 }
 
 export default Header;
