770d55f96e88a987656dcd1bd0d3128fefc8ee2f
brtvsk <vadim.nakitnyak@gmail.com>
2020-01-25 19:12:42 +0200

    Fix WinScreen

--- src/components/WinScreen.js
+++ src/components/WinScreen.js
@@ -9,15 +9,15 @@ class WinScreen extends Component {
 	
 	render() {
 		const listSongs = this.props.songs.map((song) =>
-		<SongPreview song=song />
+		<SongPreview songData={song} key={song.toString()} />
 		);
-		const winText = (this.props.won ? this.props.winner + " won on attempt " + this.props.attempt + "1" : "Nobody won(");
+		const winText = (this.props.won ? this.props.winner + " won on attempt " + this.props.attempt : "Nobody won(");
 		return (
 			<div className="win_screen">
 				<h1 className="win_screen--win_text"> { winText } </h1>
 				<h1>correct answer was:</h1>
 				<div className="win_screen--correct_song window">
-					<SongPreview song={ this.props.song } />
+					<SongPreview songData={ this.props.winner_song } />
 				</div>
 				<div className="win_screen--songs_list window scrollable">
 					{ listSongs }
