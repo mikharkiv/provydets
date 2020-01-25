e29755036a72bc4ccd513c3a68b1134b63ce1037
mikharkiv <mykhailo.postnikov@ukma.edu.ua>
2020-01-25 21:56:22 +0200

    [LAYOUT] Minor fixes, layout finished

--- src/components/WinScreen.js
+++ src/components/WinScreen.js
@@ -21,6 +21,7 @@ class WinScreen extends Component {
 				<div className="win_screen--correct_song window">
 					<SongPreview songData={ this.props.winner_song } />
 				</div>
+				<p>songs offered:</p>
 				<div className="win_screen--songs_list window scrollable">
 					{ listSongs }
 				</div>
