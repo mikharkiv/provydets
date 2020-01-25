6640e7f6dc5125aed910fb255c057aa474cc2273
dmv302 <spaceshroom175@gmail.com>
2020-01-25 20:13:18 +0200

    added player

--- src/components/AnswerScreen.js
+++ src/components/AnswerScreen.js
@@ -1,21 +1,22 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../styles/AnswerScreen.scss'
 
 class AnswerScreen extends Component {
 	constructor(props) {
 		super(props);
+		console.log(props)
 		this.wrongAnswer = this.wrongAnswer.bind(this)
 		this.rightAnswer = this.rightAnswer.bind(this)
 	}
 
-	wrongAnswer(){
+	wrongAnswer() {
 		this.props.pointUp("user")
 		this.props.callbackFromParent("Inputs")
 	}
 
-	rightAnswer(){
+	rightAnswer() {
 		this.props.pointUp("pc")
-		this.props.callbackFromParent("WinScreen",this.props.song)
+		this.props.callbackFromParent("WinScreen", this.props.song)
 	}
 
 	render() {
@@ -24,15 +25,18 @@ class AnswerScreen extends Component {
 				<div className="player window">
 					<h1 className="t_center">am i right?</h1>
 					<div className="player--image">
-						<img className="player--image--img" src={ this.props.param.songPreview } alt={ this.props.param.songTitle }/>
+						<img className="player--image--img" src={this.props.param.songPreview} alt={this.props.param.songTitle} />
 					</div>
-					<p className="t_center player--author">{ this.props.param.songAuthor }</p>
-					<p className="t_center player--title">{ this.props.param.songTitle }</p>
+					<p className="t_center player--author">{this.props.param.songAuthor}</p>
+					<p className="t_center player--title">{this.props.param.songTitle}</p>
 					<div className="player--play_button"></div>
 					<div className="player__bar" id="playerBar">
-						{/* TODO for test */}
-						<p className="player__bar--time_passed" id="playerTimePassed">0:10</p>
-						<p className="player__bar--time_left" id="playerTimeLeft">0:20</p>
+						<audio
+							controls
+							src={this.props.song.preview}>
+							Your browser does not support the
+            	<code>audio</code> element.
+    				</audio>
 					</div>
 					<div className="player--buttons">
 						<button className="button" id="answerYesButton" onClick={this.rightAnswer}>yes, right!</button>
