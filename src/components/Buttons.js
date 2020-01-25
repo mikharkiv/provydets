760fc0b2a21c9ed03fb1d310d63bddf63b2ee220
brtvsk <vadim.nakitnyak@gmail.com>
2020-01-25 13:05:05 +0200

    Add onClick functions to Buttons

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,13 +1,32 @@
 import React, {Component} from 'react';
 import Button from './Button';
 
+
 class Buttons extends Component {
+	constructor(props) {
+		super(props);
+		this.clickNewGameButton = this.clickNewGameButton.bind(this)
+		this.clickResetScores = this.clickResetScores.bind(this)
+		this.clickButtons = this.clickButtons.bind(this)
+	}
+
+	clickNewGameButton(){
+		console.log(this)
+		console.log("NEW GAME STARTS")
+	}
+	clickResetScores(){
+		console.log("RESET SCORES")
+	}
+	clickButtons(){
+		console.log("BUTTON BUTTONS WS CLICKED")
+	}
+
 	render() {
 		return (
 			<div className="button-group">
-				<Button type="new game" />
-				<Button type="reset scores" />
-				<Button type="buttons" />
+				<Button type="new game" onClick={this.clickNewGameButton} />
+				<Button type="reset scores" onClick={this.clickResetScores}/>
+				<Button type="buttons"onClick={this.clickButtons} />
 			</div>
 		)
 	}
